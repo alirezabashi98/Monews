23b01cf46cf31a0e2c810819fe4af466323edfff
Alireza Bashi <alirezabashi98@gmail.com>
2022-09-07 22:10:52 +0430

    beginning of the project

---
+++ android/app/src/main/kotlin/com/example/monews/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.monews
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
