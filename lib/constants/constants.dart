87dd170d05b557e237f88620c8c3ec47c36cf475
Alireza Bashi <alirezabashi98@gmail.com>
2022-09-07 22:34:50 +0430

    The addition of the whole program means the main colors of the program, the program font and the package of icons that are going to be used in the program b

---
+++ lib/constants/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color red = const Color(0xffFF033E);
+Color black = const Color(0xff1C1F2E);
+Color pink = const Color(0xffFFCDD8);
+Color grey = const Color(0xffBFC3C8);
+Color white = const Color(0xffFAFAFA);
